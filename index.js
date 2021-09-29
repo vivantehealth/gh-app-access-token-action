ef970c992fa96c08cbf85f91bdedaa778a9fa8a2
Reese Wilson <3253971+figadore@users.noreply.github.com>
2021-09-28 17:00:51 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+const { createAppAuth } = require("@octokit/auth-app");
+
+async function run() {
+  try {
+    const privateKey = core.getInput('private-key', { required: true });
+    const appId = core.getInput('app-id', { required: true });
+    const installationId = core.getInput('installation-id', { required: true });
+    const appAuth = createAppAuth({
+      +appId,
+      privateKey,
+    });
+    const token = appAuth({
+      type: "installation",
+      +installationId,
+    })
+    core.setSecret(token)
+    core.setOutput('token', token)
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}();
+
